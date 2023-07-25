0dc9ca0445619d490f13d74b0c670db4b23984c2
Erik Fossvold <efossvold@users.noreply.github.com>
2023-07-25 19:44:20 +0200

    Make DMG for MacOS

--- build.sh
+++ build.sh
@@ -3,10 +3,9 @@ SCRIPT_DIR=$(dirname `readlink -f "$0"`)
 BUILD_DIR=$SCRIPT_DIR/build/bin
 RELEASES_DIR=$SCRIPT_DIR/releases
 
-wails build -clean -webview2 embed -platform darwin/universal -o mac/F2SaveEditor.app
-wails build -webview2 embed -platform windows/arm64 -o win-arm64/F2SaveEditor.exe
-wails build -webview2 embed -platform windows/amd64 -o win-amd64/F2SaveEditor.exe
-
+# wails build -clean -webview2 embed -platform darwin/universal -o mac/F2SaveEditor.app
+# wails build -webview2 embed -platform windows/arm64 -o win-arm64/F2SaveEditor.exe
+# wails build -webview2 embed -platform windows/amd64 -o win-amd64/F2SaveEditor.exe
 # Cross compiling to Linux currently not supported
 # wails build -webview2 embed -platform linux/arm64 -o linux-arm64/F2SaveEditor
 # wails build -webview2 embed -platform linux/amd64 -o linux-amd64/F2SaveEditor
@@ -14,13 +13,12 @@ wails build -webview2 embed -platform windows/amd64 -o win-amd64/F2SaveEditor.ex
 rm -rf $RELEASES_DIR/*
 cd $BUILD_DIR
 zip -r $RELEASES_DIR/F2SaveEditor-mac-universal.zip F2SaveEditor.app
-echo amd64
-zip -rj $RELEASES_DIR/F2SaveEditor-win-amd64.zip $BUILD_DIR/win-amd64/F2SaveEditor.exe
-echo arm64
-zip -rj $RELEASES_DIR/F2SaveEditor-win-arm64.zip $BUILD_DIR/win-arm64/F2SaveEditor.exe
-echo mac
+# create DMG (osx)
+hdiutil create /tmp/tmp.dmg -ov -volname "F2SaveEditor" -fs HFS+ -srcfolder "$BUILD_DIR/F2SaveEditor.app"
+hdiutil convert /tmp/tmp.dmg -format UDZO -o $RELEASES_DIR/F2SaveEditor-mac-universal.dmg
 
-# Cross compiling to Linux currently not supported
+zip -rj $RELEASES_DIR/F2SaveEditor-win-amd64.zip $BUILD_DIR/win-amd64/F2SaveEditor.exe
+zip -rj $RELEASES_DIR/F2SaveEditor-win-arm64.zip $BUILD_DIR/win-arm64/F2SaveEditor.exe
 # cd $BUILD_DIR/linux-amd64
 # tar -cfz $RELEASES_DIR/F2SaveEditor-linux-amd64.tar.gz F2SaveEditor
 # cd $BUILD_DIR/linux-arm64
