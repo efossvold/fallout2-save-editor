6a9ccc1557bf2466b66dbe35328ef657702e4935
Erik Fossvold <efossvold@users.noreply.github.com>
2023-07-25 21:28:52 +0200

    Enable building again

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ SCRIPT_DIR=$(dirname `readlink -f "$0"`)
 BUILD_DIR=$SCRIPT_DIR/build/bin
 RELEASES_DIR=$SCRIPT_DIR/releases
 
-# wails build -clean -webview2 embed -platform darwin/universal -o mac/F2SaveEditor.app
-# wails build -webview2 embed -platform windows/arm64 -o win-arm64/F2SaveEditor.exe
-# wails build -webview2 embed -platform windows/amd64 -o win-amd64/F2SaveEditor.exe
+wails build -clean -webview2 embed -platform darwin/universal -o mac/F2SaveEditor.app
+wails build -webview2 embed -platform windows/arm64 -o win-arm64/F2SaveEditor.exe
+wails build -webview2 embed -platform windows/amd64 -o win-amd64/F2SaveEditor.exe
 # Cross compiling to Linux currently not supported
 # wails build -webview2 embed -platform linux/arm64 -o linux-arm64/F2SaveEditor
 # wails build -webview2 embed -platform linux/amd64 -o linux-amd64/F2SaveEditor
