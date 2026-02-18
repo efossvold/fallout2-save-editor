49e28f8a0f28986087e298e18d007f14da2d52d6
Erik Fossvold <efossvold@users.noreply.github.com>
2026-02-18 09:54:14 +0100

    Cleanup deps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,10 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-// import { analyzer,unstableRolldownAdapter } from 'vite-bundle-analyzer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // unstableRolldownAdapter(analyzer({ analyzerPort: 9002 })),
     tailwindcss(),
     react({
       babel: {
