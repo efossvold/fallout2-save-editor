8962ad95bd4bd52f93d7271cc757152cfa9d426a
Erik Fossvold <efossvold@users.noreply.github.com>
2026-07-27 20:10:51 +0200

    Update deps

--- frontend/wailsjs/runtime/runtime.js
+++ frontend/wailsjs/runtime/runtime.js
@@ -29,7 +29,7 @@ export function LogWarning(message) {
 }
 
 export function LogError(message) {
-    new window.runtime.LogError(message);
+    window.runtime.LogError(message);
 }
 
 export function LogFatal(message) {
@@ -57,7 +57,7 @@ export function EventsOnce(eventName, callback) {
 }
 
 export function EventsEmit(eventName) {
-    const args = [eventName].slice.call(arguments);
+    let args = [eventName].slice.call(arguments);
     return window.runtime.EventsEmit.apply(null, args);
 }
 
