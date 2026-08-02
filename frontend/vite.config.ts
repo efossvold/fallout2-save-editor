4d803ae76fe24476e3558126fc66c68cf020c272
Erik Fossvold <efossvold@users.noreply.github.com>
2026-08-02 11:39:38 +0200

    Don't load CSS async, causes FOUC

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,18 +13,6 @@ export default defineConfig({
     // DevTools(),
     panda(),
     octane(),
-    {
-      name: 'async-css-links',
-      transformIndexHtml(html) {
-        // Rewrites the link tag to async
-        // https://scottjehl.com/posts/async-css-already/
-        return html.replace(
-          // oxlint-disable-next-line prefer-named-capture-group
-          /<link rel="stylesheet" crossorigin href="(.*?)">/g,
-          '<link rel="stylesheet" crossorigin href="$1" media="print" onload="this.media=\'all\'">',
-        )
-      },
-    },
   ],
   build: {
     rolldownOptions: {
